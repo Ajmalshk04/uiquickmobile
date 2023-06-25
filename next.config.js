ec101a7c8d2ba93a7bbbb31e39ef09a6c7aa79df
Ajmalshaikh04 <ajshaikh0404@gmail.com>
2023-06-25 13:01:37 +0530

    fixed next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,26 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   optimizeFonts: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.webp$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            name: '[name].[hash].[ext]',
+            outputPath: 'static/images/',
+            publicPath: '/_next/static/images/',
+            esModule: false,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
+
