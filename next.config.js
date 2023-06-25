eba05166ec3acdd94db3f1d3ef1e5170a05263ad
Ajmalshaikh04 <ajshaikh0404@gmail.com>
2023-06-25 13:05:28 +0530

    fixed next config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,43 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  optimizeFonts: true,
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   optimizeFonts: true,
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.webp$/,
+//       use: [
+//         {
+//           loader: 'url-loader',
+//           options: {
+//             limit: 8192,
+//             name: '[name].[hash].[ext]',
+//             outputPath: 'static/images/',
+//             publicPath: '/_next/static/images/',
+//             esModule: false,
+//           },
+//         },
+//       ],
+//     });
+
+//     return config;
+//   },
+// }
+
+// module.exports = nextConfig
+
+module.exports = {
   webpack(config) {
     config.module.rules.push({
-      test: /\.webp$/,
+      test: /\.(png|jpe?g|gif|svg|webp)$/i,
       use: [
         {
           loader: 'url-loader',
           options: {
-            limit: 8192,
+            limit: 8192, // Adjust the limit as needed
             name: '[name].[hash].[ext]',
-            outputPath: 'static/images/',
-            publicPath: '/_next/static/images/',
+            outputPath: 'static/images/', // Output path for processed images
+            publicPath: '/_next/static/images/', // Public path for images
             esModule: false,
           },
         },
@@ -22,7 +46,4 @@ const nextConfig = {
 
     return config;
   },
-}
-
-module.exports = nextConfig
-
+};
